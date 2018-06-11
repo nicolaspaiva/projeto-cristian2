785fd83aeabfaae7a5238129eccc5d1da718a231
Nicolas Paiva <nicolas.paiva1@gmail.com>
2018-06-10 22:21:05 -0300

    Terminei o tema de inverno e default

--- Projeto PA/public_html/js/winter-header.js
+++ Projeto PA/public_html/js/winter-header.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 Snowy();
 function Snowy() {
     var snow, arr = [];
-    var num = 50, tsc = 1, sp = 1;
+    var num = 20, tsc = 1, sp = 1;
     var sc = 1.3, t = 0, mv = 20, min = 1;
     for (var i = 0; i < num; ++i) {
         snow = new Flake();
